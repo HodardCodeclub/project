6b0194d72184a5b3729931ff1a04ebc23b1e8827
hodard <hodardhazwinayo@gmail.com>
2018-04-26 12:05:17 +0200

    credential password edited

--- admin/include/connnect.php
+++ admin/include/connnect.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","","gakinjiro");
+$con=mysqli_connect("localhost","root","lecteur1994ange","gakinjiro");
 
 if(mysqli_connect_errno()){
 	echo "Failed to connect".mysqli_connect_error();
